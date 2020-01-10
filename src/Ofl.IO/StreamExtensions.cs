7b9b41cb8874b63e94e4ac8d26eeab1aba4fb0d8
casperOne <casperOne@caspershouse.com>
2020-01-10 17:26:40 -0500

    Intermediate check in for .NET Standard 2.1 migration.

--- src/Ofl.IO/StreamExtensions.cs
+++ src/Ofl.IO/StreamExtensions.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Buffers;
+using System.Collections.Generic;
 using System.IO;
-using System.Linq;
+using System.Threading;
 
 namespace Ofl.IO
 {
@@ -9,9 +11,55 @@ namespace Ofl.IO
         public static readonly int DefaultToAsyncEnumerableBufferSize = 4096;
 
         public static IAsyncEnumerable<byte> ToAsyncEnumerable(this Stream stream) =>
-            stream.ToAsyncEnumerable(DefaultToAsyncEnumerableBufferSize);
+            stream.ToAsyncEnumerable(DefaultToAsyncEnumerableBufferSize, ArrayPool<byte>.Shared);
 
-        public static IAsyncEnumerable<byte> ToAsyncEnumerable(this Stream stream, int bufferSize) =>
-            AsyncEnumerable.CreateEnumerable(() => new StreamAsyncEnumerator(stream, bufferSize));
+        public static IAsyncEnumerable<byte> ToAsyncEnumerable(
+            this Stream stream, 
+            int bufferSize,
+            ArrayPool<byte> arrayPool,
+            CancellationToken cancellationToken
+        )
+        {
+            // Validate parameters.
+            if (stream == null) throw new ArgumentNullException(nameof(stream));
+            if (bufferSize <= 0)
+                throw new ArgumentOutOfRangeException(nameof(bufferSize), 
+                    $"The { nameof(bufferSize) } parameter must be a positive value.");
+            if (arrayPool == null) throw new ArgumentNullException(nameof(arrayPool));
+
+            // The implementation.
+            async IAsyncEnumerable<byte> Implementation() {
+                // The buffer.
+                byte[] buffer = null;
+
+                // Wrap in a try/finalize.
+                try
+                {
+                    // Allocate.
+                    buffer = arrayPool.Rent(bufferSize);
+
+                    // The bytes read.
+                    int read;
+
+                    // Keep reading from the stream in the allocated amounts.
+                    while ((read = await stream.ReadAsync(buffer, 0, bufferSize, cancellationToken)
+                        .ConfigureAwait(false)) != 0)
+                    {
+                        // Cycle through the bytes and yield.
+                        for (int index = 0; index < read; ++index)
+                            yield return buffer[index];
+                    }
+                }
+                finally
+                {
+                    // Return.
+                    if (buffer != null)
+                        arrayPool.Return(buffer);
+                }
+            }
+
+            // Return the implementation.
+            return Implementation();
+        }
     }
 }
--- src/Ofl.IO/TextReaderExtensions.cs
+++ src/Ofl.IO/TextReaderExtensions.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Threading;
 
 namespace Ofl.IO
 {
@@ -12,40 +13,42 @@ namespace Ofl.IO
             // Validate parameters.
             if (textReader == null) throw new ArgumentNullException(nameof(textReader));
 
+            IEnumerable<string> Implementation() {
+                // The line.
+                string line;
+
+                // Cycle through readline.  While there's a value, yield.
+                while ((line = textReader.ReadLine()) != null)
+                    yield return line;
+            }
+
             // Call the implementation.
-            return textReader.GetEnumerableImplementation();
+            return Implementation();
         }
 
-        private static IEnumerable<string> GetEnumerableImplementation(this TextReader textReader)
-        {
-            // Validate parameters.
-            if (textReader == null) throw new ArgumentNullException(nameof(textReader));
-
-            // The line.
-            string line;
-
-            // Cycle through readline.  While there's a value, yield.
-            while ((line = textReader.ReadLine()) != null)
-                yield return line;
-        }
-
-        public static IAsyncEnumerable<string> GetAsyncEnumerable(this TextReader reader)
+        public static IAsyncEnumerable<string> GetAsyncEnumerable(
+            this TextReader reader,
+            CancellationToken cancellationToken
+        )
         {
             // Validate parameters.
             if (reader == null) throw new ArgumentNullException(nameof(reader));
 
-            // The line.
-            string line = null;
+            // The implementation.
+            async IAsyncEnumerable<string> Implementation() {
+                // The line.
+                string line;
 
-            // Create the enumerator.
-            return AsyncEnumerable.CreateEnumerable(() =>
-                AsyncEnumerable.CreateEnumerator(async ct => {
-                    // Read the line.
-                    line = await reader.ReadLineAsync().ConfigureAwait(false);
+                // While the line is not null.
+                while (!cancellationToken.IsCancellationRequested &&
+                    (line = await reader.ReadLineAsync().ConfigureAwait(false)) != null
+                )
+                    // Yield
+                    yield return line;
+            }
 
-                    // If the line is null, stop reading.
-                    return line != null;
-                }, () => line, () => { }));
+            // Return the implementation.
+            return Implementation();
         }
     }
 }
